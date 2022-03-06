1a673980791b7e770f1f8c683bced5635b3955d8
Minhdn1 <minhdn1@vmodev.com>
2022-03-06 21:19:19 +0700

    CLK32-876: Update View

---
+++ ClearKeep/Scenes/Home/Register/RegisterView.swift
@@ -0,0 +1,57 @@
+//
+//  RegisterView.swift
+//  ClearKeep
+//
+//  Created by MinhDev on 04/03/2022.
+//
+
+import SwiftUI
+import CommonUI
+
+private enum Constants {
+	static let widthReView = UIScreen.main.bounds.width - 20
+	static let spacer = 50.0
+	static let heightLogo = 120.0
+	static let widthLogo = 160.0
+	static let spacing = 20.0
+}
+
+struct RegisterView: View {
+// MARK: - Variables
+	@Environment(\.colorScheme) var colorScheme
+
+// MARK: - Body
+	var body: some View {
+		ZStack {
+			ScrollView {
+				Spacer(minLength: Constants.spacer)
+				VStack(spacing: Constants.spacing) {
+					imageLogo
+						.resizable()
+						.aspectRatio(contentMode: .fit)
+						.frame(width: Constants.widthLogo, height: Constants.heightLogo)
+					RegisterContentView(username: "Test", password: "123", displayname: "Minh", rePassword: "123", inputStyle: .normal)
+						.frame(width: Constants.widthReView)
+				}
+			}
+		}
+		.frame(maxWidth: .infinity, maxHeight: .infinity)
+		.background(backgroundColor)
+		.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+	}
+}
+// MARK: - Private func
+private extension RegisterView {
+	var imageLogo: Image {
+		AppTheme.shared.imageSet.logo
+	}
+	var backgroundColor: Color {
+		colorScheme == .light ? AppTheme.shared.colorSet.primaryDefault : AppTheme.shared.colorSet.black
+	}
+}
+// MARK: - Preview
+struct RegisterView_Previews: PreviewProvider {
+	static var previews: some View {
+		RegisterView()
+	}
+}
---
+++ ClearKeep/Scenes/Home/Register/Views/RegisterContentView.swift
@@ -0,0 +1,99 @@
+//
+//  RegisterContentView.swift
+//  ClearKeep
+//
+//  Created by MinhDev on 04/03/2022.
+//
+
+import SwiftUI
+import CommonUI
+import RealmSwift
+
+private enum Constants {
+	static let width = 120.0
+	static let height = 40.0
+	static let radius = 20.0
+	static let sapcing = 20.0
+}
+
+struct RegisterContentView: View {
+	// MARK: - Variables
+	@Environment(\.colorScheme) var colorScheme
+	@State var username: String
+	@State var password: String
+	@State var displayname: String
+	@State var rePassword: String
+	@State var focused: Bool = false
+	@State var inputStyle: TextInputStyle
+	// MARK: - Body
+	var body: some View {
+		GroupBox(label:
+					Text("Register.Title")
+					.font(fontTitle)) {
+			VStack(alignment: .center, spacing: Constants.sapcing) {
+				CommonTextField(text: $username, inputStyle: $inputStyle, inputIcon: iconMail, placeHolder: "General.Email", keyboardType: .default, onEditingChanged: { isEditing in
+					if isEditing {
+						inputStyle = .normal
+					} else {
+						inputStyle = .highlighted
+					}
+				})
+				CommonTextField(text: $displayname, inputStyle: $inputStyle, inputIcon: iconUser, placeHolder: "General.Displayname", keyboardType: .default, onEditingChanged: { isEditing in
+					if isEditing {
+						inputStyle = .highlighted
+					} else {
+						inputStyle = .normal
+					}
+				})
+				SecureTextField(secureText: $password, inputStyle: $inputStyle, inputIcon: iconSecure, placeHolder: "General.Password", keyboardType: .default )
+				SecureTextField(secureText: $password, inputStyle: $inputStyle, inputIcon: iconSecure, placeHolder: "General.RePassword", keyboardType: .default )
+				HStack {
+					Button("Register.SignIn") {
+
+					}
+					.foregroundColor(foregroundColorPrimary)
+					Spacer()
+					Button("Register.SignUp") {
+
+					}
+					.frame(width: Constants.width, height: Constants.height)
+					.background(LinearGradient(gradient: Gradient(colors: backgroundColorView), startPoint: .leading, endPoint: .trailing))
+					.foregroundColor(foregroundColorWhite)
+					.cornerRadius(Constants.radius)
+				}
+			}
+		}
+	}
+}
+
+// MARK: - Private func
+private extension RegisterContentView {
+	var backgroundColorView: [Color] {
+		AppTheme.shared.colorSet.gradientPrimary
+	}
+	var foregroundColorWhite: Color {
+		AppTheme.shared.colorSet.offWhite
+	}
+	var foregroundColorPrimary: Color {
+		AppTheme.shared.colorSet.primaryDefault
+	}
+	var iconSecure: Image {
+		AppTheme.shared.imageSet.lockIcon
+	}
+	var iconUser: Image {
+		AppTheme.shared.imageSet.userIcon
+	}
+	var iconMail: Image {
+		AppTheme.shared.imageSet.mailIcon
+	}
+	var fontTitle: Font {
+		AppTheme.shared.fontSet.font(style: .body1)
+	}
+}
+// MARK: - Preview
+struct RegisterContentView_Previews: PreviewProvider {
+	static var previews: some View {
+		RegisterContentView(username: "Test", password: "123", displayname: "Minh", rePassword: "123", inputStyle: .normal)
+
+	}
+}
