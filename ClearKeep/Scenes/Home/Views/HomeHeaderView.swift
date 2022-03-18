5a83eec9c969eb20052ca8c8d8d47f3bb622ca4b
Minhdn1 <minhdn1@vmodev.com>
2022-03-18 15:10:14 +0700

    CLK32-884: UpdateView

--- ClearKeep/Scenes/Home/Views/HomeHeaderView.swift
+++ ClearKeep/Scenes/Home/Views/HomeHeaderView.swift
@@ -10,28 +10,26 @@ import Common
 import CommonUI
 
 private enum Constants {
-	static let radius = 40.0
-	static let spacing = 10.0
-	static let paddingTop = 50.0
 	static let paddingLeading = 100.0
 	static let padding = 20.0
-	static let sizeImage = 56.0
-	static let sizeCircle = 12.0
 	static let sizeOffset = 30.0
 	static let sizeIcon = 24.0
-	static let sizeDisable = 12.0
 }
+
 struct HomeHeaderView: View {
 	// MARK: - Variables
 	@Environment(\.colorScheme) var colorScheme
 	@Binding var searchText: String
 	@Binding var inputStyle: TextInputStyle
-	@State var isMenuAction: Bool
-	@State private var offset = CGFloat.zero
-	@State private var closeOffset = CGFloat.zero
-	@State private var openOffset = CGFloat.zero
+	@State private(set) var isMenuAction: Bool
 	// MARK: - Init
-	
+	init(searchText: Binding<String>,
+		 inputStyle: Binding<TextInputStyle>,
+		 isMenuAction: Bool) {
+		self._searchText = searchText
+		self._inputStyle = inputStyle
+		self._isMenuAction = .init(initialValue: isMenuAction)
+	}
 	// MARK: - Body
 	var body: some View {
 		NavigationView {
@@ -49,6 +47,7 @@ private extension HomeHeaderView {
 	var content: AnyView {
 		AnyView(contentView)
 	}
+
 	var header: AnyView {
 		AnyView(headerView)
 	}
@@ -59,22 +58,31 @@ private extension HomeHeaderView {
 	var backgroundColorView: Color {
 		colorScheme == .light ? AppTheme.shared.colorSet.offWhite : AppTheme.shared.colorSet.black
 	}
+
 	var backgroundColor: LinearGradient {
 		colorScheme == .light ? backgroundColorGradient : backgroundColorBlack
 	}
+
 	var backgroundColorBlack: LinearGradient {
 		LinearGradient(gradient: Gradient(colors: AppTheme.shared.colorSet.gradientBlack), startPoint: .leading, endPoint: .trailing)
 	}
+
 	var backgroundColorGradient: LinearGradient {
 		LinearGradient(gradient: Gradient(colors: AppTheme.shared.colorSet.gradientLinear), startPoint: .leading, endPoint: .trailing)
 	}
+
+	var foregroundColorTitle: Color {
+		colorScheme == .light ? AppTheme.shared.colorSet.black : AppTheme.shared.colorSet.greyLight
+	}
 }
+
 // MARK: - Private func
 private extension HomeHeaderView {
 	func menuAction() {
 		self.isMenuAction.toggle()
 	}
 }
+
 // MARK: - Displaying Content
 private extension HomeHeaderView {
 	var contentView: some View {
@@ -82,23 +90,26 @@ private extension HomeHeaderView {
 			ZStack {
 				ServerView(isChangeSever: false)
 				header
-				HomeMenuView(userName: "Test", urlString: "test", expand: false, show: true, isChangeStatus: true, isMenuAction: $isMenuAction)
+				HomeMenuView(userName: "Test", urlString: "test", isExpand: false, isShow: true, isChangeStatus: true, isMenuAction: $isMenuAction)
 					.frame(width: geometry.size.width)
 					.offset(x: self.isMenuAction ? 0 : geometry.size.width )
 					.animation(.default)
 			}
 		}
 	}
+
 	var headerView: some View {
 		VStack {
 			HStack {
 				Text("Home.Title".localized)
 					.font(AppTheme.shared.fontSet.font(style: .display3))
+					.foregroundColor(foregroundColorTitle)
 				Spacer()
 				Button(action: menuAction) {
 					AppTheme.shared.imageSet.menuIcon
 						.resizable()
 						.frame(width: Constants.sizeIcon, height: Constants.sizeIcon)
+						.foregroundColor(foregroundColorTitle)
 				}
 			}
 			SearchTextField(searchText: $searchText,
@@ -114,6 +125,7 @@ private extension HomeHeaderView {
 		.padding(.trailing, Constants.padding)
 	}
 }
+
 // MARK: - Preview
 #if DEBUG
 struct HomeHeaderView_Previews: PreviewProvider {
