91f2ab07370061d69e9774f0eb7b9a9fd1ccbced
dongnh99 <dongnh@vmodev.com>
2022-04-02 00:19:04 +0700

    CLK32-902:Add Setting Server

---
+++ ClearKeep/Scenes/Settings/SettingServerInMemoryStore.swift
@@ -0,0 +1,14 @@
+//
+//  SettingServerInMemoryStore.swift
+//  ClearKeep
+//
+//  Created by đông on 02/04/2022.
+//
+
+import Foundation
+
+protocol ISettingServerInMemoryStore {}
+
+struct SettingServerInMemoryStore { }
+
+extension SettingServerInMemoryStore: ISettingServerInMemoryStore {}
---
+++ ClearKeep/Scenes/Settings/SettingServerView.swift
@@ -0,0 +1,8 @@
+//
+//  SettingServerView.swift
+//  ClearKeep
+//
+//  Created by đông on 02/04/2022.
+//
+
+import Foundation
