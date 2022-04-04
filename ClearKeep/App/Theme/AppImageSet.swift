f5e1c31fbe406f69315c2be16babf0c63b8107ef
Minhdn1 <minhdn1@vmodev.com>
2022-04-04 10:24:14 +0700

    CLK32-906: UpdateView

--- ClearKeep/App/Theme/AppImageSet.swift
+++ ClearKeep/App/Theme/AppImageSet.swift
@@ -68,8 +68,8 @@ struct AppImageSet: IAppImageSet {
 	var googleIcon: Image { Image("ic_google") }
 	var officeIcon: Image { Image("ic_office") }
 	var facebookIcon: Image { Image("ic_faceBook") }
-	var checkMask: Image { Image(systemName: "circle.fill") }
-	var checkMaskFill: Image { Image(systemName: "checkmark.circle.fill") }
+	var checkMask: Image { Image("ic_ellipse") }
+	var checkMaskFill: Image { Image("ic_checkbox") }
 }
 
 extension AppImageSet: ICommonUIImageSet {
