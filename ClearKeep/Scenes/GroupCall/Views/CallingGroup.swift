404d40a93e1598dc1b47f39c4b8a813c0ea9714a
dongnh99 <dongnh@vmodev.com>
2022-04-08 10:33:25 +0700

    CLK32-916:Add GroupVoiceCall view

---
+++ ClearKeep/Scenes/GroupCall/Views/CallingGroup.swift
@@ -0,0 +1,232 @@
+//
+//  CallingGroup.swift
+//  ClearKeep
+//
+//  Created by đông on 08/04/2022.
+//
+
+import SwiftUI
+import Combine
+import Common
+import CommonUI
+
+private enum Constant {
+	static let spacerTopView = 90.0
+	static let spacer = 25.0
+	static let spacerBottom = 45.0
+	static let paddingVertical = 14.0
+	static let sizeImage = 120.0
+	static let paddingButtonNext = 60.0
+	static let borderLineWidth = 2.0
+	static let opacity = 0.2
+}
+
+struct CallingGroup: View {
+	// MARK: - Variables
+	let inspection = ViewInspector<Self>()
+	@Environment(\.injected) private var injected: DIContainer
+	@Environment(\.colorScheme) private var colorScheme
+	@Environment(\.presentationMode) private var presentationMode: Binding<PresentationMode>
+	@State private(set) var samples: Loadable<[ICallModel]>
+	@State private(set) var isTappedMute: Bool = false
+	@State private(set) var isTappedCamera: Bool = false
+	@State private(set) var isTappedSpeaker: Bool = false
+	@State private(set) var isTappedCancel: Bool = false
+
+	// MARK: - Init
+	public init(samples: Loadable<[ICallModel]> = .notRequested) {
+		self._samples = .init(initialValue: samples)
+	}
+
+	// MARK: - Body
+	var body: some View {
+		content
+			.onReceive(inspection.notice) { inspection.visit(self, $0) }
+			.edgesIgnoringSafeArea(.all)
+			.navigationBarBackButtonHidden(true)
+	}
+}
+
+// MARK: - Private
+private extension CallingGroup {
+	var content: AnyView {
+		AnyView(notRequestedView)
+	}
+}
+
+// MARK: - Loading Content
+private extension CallingGroup {
+	var notRequestedView: some View {
+		VStack(spacing: Constant.spacer) {
+			statusCalling
+				.padding(.top, Constant.spacerTopView)
+			userName
+			supportCalling
+				.padding(.top, Constant.spacerBottom)
+			buttonCancel
+				.padding(.bottom, Constant.paddingButtonNext)
+			Spacer()
+		}
+		.background(backgroundGradientPrimary)
+		.padding(.horizontal, Constant.paddingVertical)
+	}
+
+	var statusCalling: some View {
+		Text("Call.Calling".localized)
+			.padding(.all)
+			.font(AppTheme.shared.fontSet.font(style: .input2))
+			.foregroundColor(foregroundColorGrey5)
+			.frame(maxWidth: .infinity, alignment: .center)
+	}
+
+	var userName: some View {
+		Text("UI Designs".localized)
+			.frame(maxWidth: .infinity, alignment: .center)
+			.font(AppTheme.shared.fontSet.font(style: .display2))
+			.foregroundColor(foregroundColorWhite)
+	}
+
+	var supportCalling: some View {
+		HStack {
+			Button {
+				isTappedMute.toggle()
+			} label: {
+				ZStack {
+					Circle()
+						.strokeBorder(foregroundColorWhite, lineWidth: Constant.borderLineWidth)
+						.background(Circle().foregroundColor(backgroundButtonMute))
+						.frame(width: Constant.paddingButtonNext, height: Constant.paddingButtonNext)
+					muteIcon
+						.renderingMode(.template)
+						.foregroundColor(foregroundButtonMute)
+				}
+				.frame(maxWidth: .infinity)
+			}
+
+			Button {
+				isTappedCamera.toggle()
+			} label: {
+				ZStack {
+					Circle()
+						.strokeBorder(foregroundColorWhite, lineWidth: Constant.borderLineWidth)
+						.background(Circle().foregroundColor(backgroundButtonCamera))
+						.frame(width: Constant.paddingButtonNext, height: Constant.paddingButtonNext)
+					cameraIcon
+						.renderingMode(.template)
+						.foregroundColor(foregroundButtonCamera)
+				}
+				.frame(maxWidth: .infinity)
+			}
+
+			Button {
+				isTappedSpeaker.toggle()
+			} label: {
+				ZStack {
+					Circle()
+						.strokeBorder(foregroundColorWhite, lineWidth: Constant.borderLineWidth)
+						.background(Circle().foregroundColor(backgroundButtonSpeaker))
+						.frame(width: Constant.paddingButtonNext, height: Constant.paddingButtonNext)
+					speakerIcon
+						.renderingMode(.template)
+						.foregroundColor(foregroundButtonSpeaker)
+				}
+			}
+			.frame(maxWidth: .infinity)
+		}
+		.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+	}
+
+	var buttonCancel: some View {
+		VStack {
+			Button {
+				isTappedCancel.toggle()
+			} label: {
+				ZStack {
+					Circle()
+						.strokeBorder(backgroundEndCall, lineWidth: Constant.borderLineWidth)
+						.background(Circle().foregroundColor(backgroundEndCall))
+						.frame(width: Constant.paddingButtonNext, height: Constant.paddingButtonNext)
+					AppTheme.shared.imageSet.phoneOffIcon
+						.renderingMode(.template)
+						.foregroundColor(foregroundColorWhite)
+				}
+		}
+			Text("Call.Cancel".localized)
+				.font(AppTheme.shared.fontSet.font(style: .body2))
+				.foregroundColor(foregroundColorWhite)
+		}
+	}
+}
+
+// MARK: - Color func
+private extension CallingGroup {
+	var foregroundColorWhite: Color {
+		AppTheme.shared.colorSet.offWhite
+	}
+
+	var foregroundColorBlack: Color {
+		AppTheme.shared.colorSet.black
+	}
+
+	var foregroundColorGrey5: Color {
+		AppTheme.shared.colorSet.grey5
+	}
+
+	var foregroundColorGreyLight: Color {
+		AppTheme.shared.colorSet.greyLight
+	}
+
+	var foregroundCrossIcon: Color {
+		colorScheme == .light ? foregroundColorBlack : foregroundColorGreyLight
+	}
+
+	var backgroundGradientPrimary: LinearGradient {
+		LinearGradient(gradient: Gradient(colors: AppTheme.shared.colorSet.gradientPrimary), startPoint: .leading, endPoint: .trailing)
+	}
+
+	var backgroundEndCall: Color {
+		AppTheme.shared.colorSet.errorDefault
+	}
+
+	var backgroundButtonMute: Color {
+		isTappedMute == true ? foregroundColorWhite : foregroundColorWhite.opacity(0)
+	}
+
+	var foregroundButtonMute: Color {
+		isTappedMute == true ? foregroundColorBlack : foregroundColorWhite
+	}
+
+	var backgroundButtonCamera: Color {
+		isTappedCamera == true ? foregroundColorWhite : foregroundColorWhite.opacity(0)
+	}
+
+	var foregroundButtonCamera: Color {
+		isTappedCamera == true ? foregroundColorBlack : foregroundColorWhite
+	}
+
+	var backgroundButtonSpeaker: Color {
+		isTappedSpeaker == true ? foregroundColorWhite : foregroundColorWhite.opacity(0)
+	}
+
+	var foregroundButtonSpeaker: Color {
+		isTappedSpeaker == true ? foregroundColorBlack : foregroundColorWhite
+	}
+
+	var muteIcon: Image {
+		isTappedMute ? AppTheme.shared.imageSet.microphoneOffIcon : AppTheme.shared.imageSet.microPhoneIcon
+	}
+
+	var cameraIcon: Image {
+		isTappedCamera ? AppTheme.shared.imageSet.videoOffIcon : AppTheme.shared.imageSet.videoIcon
+	}
+
+	var speakerIcon: Image {
+		isTappedSpeaker ? AppTheme.shared.imageSet.speakerOffIcon : AppTheme.shared.imageSet.speakerIcon
+	}
+}
+
+struct CallingGroup_Previews: PreviewProvider {
+    static var previews: some View {
+        CallingGroup()
+    }
+}
