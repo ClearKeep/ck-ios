7b22dda71090adafcce3e054d3c65245b945c860
NamNH <unknown>
2022-04-14 15:40:26 +0700

    Add API Service

---
+++ Packages/Networking/Networking/Classes/Models/IUser.swift
@@ -0,0 +1,19 @@
+//
+//  IUser.swift
+//  Networking
+//
+//  Created by NamNH on 13/04/2022.
+//
+
+import Foundation
+
+public protocol IUser {
+	var id: String { get }
+	var name: String { get }
+}
+
+public protocol IOwner {
+	var clientId: String { get }
+	var clientUserName: String { get }
+	var domain: String { get }
+}
