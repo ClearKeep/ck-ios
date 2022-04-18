1a2209482b8be0560544e06ce648169e0ddf9188
dongnh99 <dongnh@vmodev.com>
2022-04-18 15:23:07 +0700

    CLK32-927:Add TwoFactor Logic

--- ClearKeep/Scenes/TwoFactor/TwoFactorInteractor.swift
+++ ClearKeep/Scenes/TwoFactor/TwoFactorInteractor.swift
@@ -6,13 +6,18 @@
 //
 
 import Common
+import ChatSecure
 
 protocol ITwoFactorInteractor {
 	var worker: ITwoFactorWorker { get }
+	func signIn(passcode: String) async
 }
 
 struct TwoFactorInteractor {
 	let appState: Store<AppState>
+	let channelStorage: IChannelStorage
+	let socialAuthenticationService: ISocialAuthenticationService
+	let authenticationService: IAuthenticationService
 }
 
 extension TwoFactorInteractor: ITwoFactorInteractor {
@@ -20,5 +25,9 @@ extension TwoFactorInteractor: ITwoFactorInteractor {
 		let remoteStore = TwoFactorRemoteStore()
 		let inMemoryStore = TwoFactorInMemoryStore()
 		return TwoFactorWorker(remoteStore: remoteStore, inMemoryStore: inMemoryStore)
+	}
+
+	func signIn(passcode: String) async {
+		
 	}
 }
--- ClearKeep/Scenes/TwoFactor/TwoFactorRemoteStore.swift
+++ ClearKeep/Scenes/TwoFactor/TwoFactorRemoteStore.swift
@@ -6,12 +6,28 @@
 //
 
 import Foundation
+import Combine
+import ChatSecure
 
 protocol ITwoFactorRemoteStore {
+	func signIn(userId: String, otp: String, otpHash: String, haskKey: String, domain: String) async -> Result<String, Error>
 }
 
 struct TwoFactorRemoteStore {
+	let socialAuthenticationService: ISocialAuthenticationService
+	let authenticationService: IAuthenticationService
 }
 
 extension TwoFactorRemoteStore: ITwoFactorRemoteStore {
+	func signIn(userId: String, otp: String, otpHash: String, haskKey: String, domain: String) async -> Result<String, Error> {
+		let response = await authenticationService.validateOTP(userId: userId, otp: otp, otpHash: otpHash, haskKey: haskKey, domain: domain)
+		switch response {
+		case .success(let data):
+			print(data)
+			return .success("")
+		case .failure(let error):
+			return .failure(error)
+		}
+	}
+
 }
