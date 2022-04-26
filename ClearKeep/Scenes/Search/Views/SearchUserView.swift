b9f2a6b433fd4a173036107ffb0f66e662175bc1
Minhdn1 <minhdn1@vmodev.com>
2022-04-26 11:09:48 +0700

    CLK32-903: UpdateView

--- ClearKeep/Scenes/Search/Views/SearchUserView.swift
+++ ClearKeep/Scenes/Search/Views/SearchUserView.swift
@@ -69,7 +69,7 @@ private extension SearchUserView {
 #if DEBUG
 struct SearchUserView_Previews: PreviewProvider {
 	static var previews: some View {
-		SearchUserView(model: )
+		SearchUserView()
 	}
 }
 #endif
