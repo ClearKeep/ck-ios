bca146069782cc15394e85a101910ba42f9da8c7
dongnh99 <dongnh@vmodev.com>
2022-05-11 11:09:03 +0700

    CLK32-906: Add Missing screen Create Group Chat UI

--- ClearKeep/Scenes/ChatGroup/Views/ListUser.swift
+++ ClearKeep/Scenes/ChatGroup/Views/ListUser.swift
@@ -16,14 +16,10 @@ private enum Constants {
 struct ListUser: View {
 	// MARK: - Variables
 	@State private var selectedCheckbox: Bool = false
-	@Binding var name: String
 	@Environment(\.colorScheme) var colorScheme
 	@Environment(\.injected) private var injected: DIContainer
-
-	// MARK: - Init
-	init(name: Binding<String>) {
-		self._name = name
-	}
+	var userModel: GroupChatModel
+	@ObservedObject var groupData: GroupChatData
 
 	// MARK: - Body
 	var body: some View {
@@ -34,23 +30,43 @@ struct ListUser: View {
 						.frame(width: Constants.sizeImage, height: Constants.sizeImage)
 					AppTheme.shared.imageSet.userIcon
 				}
-				Text(name)
+				Text(userModel.name)
 					.frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
 					.font(AppTheme.shared.fontSet.font(style: .input2))
 					.foregroundColor(foregroundTagUser)
-				CheckBoxButtons(text: "", isChecked: $selectedCheckbox)
+
+				Button(action: {
+					selectedCheckbox.toggle()
+					groupData.setCheckItem(model: userModel, isChecked: selectedCheckbox)
+				}, label: {
+					checkMaskIcon
+						.foregroundColor(focegroundColorImage)
+				})
+			}.onAppear {
+				selectedCheckbox = userModel.checked
 			}
 	}
 }
 
 // MARK: - Private Variables
 private extension ListUser {
-
 	var foregroundTagUser: Color {
 		colorScheme == .light ? AppTheme.shared.colorSet.grey2 : AppTheme.shared.colorSet.greyLight
 	}
 
 	var backgroundGradientPrimary: LinearGradient {
 		LinearGradient(gradient: Gradient(colors: AppTheme.shared.colorSet.gradientPrimary), startPoint: .leading, endPoint: .trailing)
+	}
+
+	var focegroundColorImage: Color {
+		selectedCheckbox ? AppTheme.shared.colorSet.primaryDark : AppTheme.shared.colorSet.grey3
+	}
+
+	var font: Font {
+		AppTheme.shared.fontSet.font(style: .body3)
+	}
+
+	var checkMaskIcon: Image {
+		selectedCheckbox ? AppTheme.shared.imageSet.checkedIcon : AppTheme.shared.imageSet.unCheckIcon
 	}
 }
