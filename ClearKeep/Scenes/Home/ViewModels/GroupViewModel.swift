907f9fff6c6b195dadd2e5c931219f2e9406888f
minh5896 <100137499+Minhdn1@users.noreply.github.com>
2022-06-07 14:49:58 +0700

    [CLK32-1008]: fix comment

--- ClearKeep/Scenes/Home/ViewModels/GroupViewModel.swift
+++ ClearKeep/Scenes/Home/ViewModels/GroupViewModel.swift
@@ -8,23 +8,6 @@
 import Foundation
 import Model
 
-struct GroupViewModel: Identifiable {
-	var id: String { "\(groupId)" }
-	var groupId: Int64
-	var groupName: String
-	var groupAvatar: String
-	var groupType: String
-	var hasUnreadMessage: Bool = false
-	
-	init(_ model: IGroupModel) {
-		self.groupId = model.groupId
-		self.groupName = model.groupName
-		self.groupAvatar = model.groupAvatar
-		self.groupType = model.groupType
-		self.hasUnreadMessage = model.hasUnreadMessage
-	}
-}
-
 protocol IGroupViewModels {
 	var viewModelGroup: [IGroupModel] { get }
 }
@@ -33,5 +16,22 @@ struct GroupViewModels: IGroupViewModels {
 	var viewModelGroup: [IGroupModel]
 	init(_ model: IHomeModels) {
 		self.viewModelGroup = model.groupModel ?? [IGroupModel]()
+	}
+}
+
+struct GroupViewModel: Identifiable {
+	var id: String { "\(groupId)" }
+	var groupId: Int64
+	var groupName: String
+	var groupAvatar: String
+	var groupType: String
+	var hasUnreadMessage: Bool = false
+
+	init(_ model: IGroupModel) {
+		self.groupId = model.groupId
+		self.groupName = model.groupName
+		self.groupAvatar = model.groupAvatar
+		self.groupType = model.groupType
+		self.hasUnreadMessage = model.hasUnreadMessage
 	}
 }
