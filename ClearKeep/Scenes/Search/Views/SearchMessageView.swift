e125dc3d4b59777da453c017f24ec8154d87bb52
minh5896 <100137499+Minhdn1@users.noreply.github.com>
2022-08-05 13:33:27 +0700

    [CLK32-1188]: fix back button to return search screen
    Unable to tap back button to return search screen

--- ClearKeep/Scenes/Search/Views/SearchMessageView.swift
+++ ClearKeep/Scenes/Search/Views/SearchMessageView.swift
@@ -83,33 +83,6 @@ private extension SearchMessageView {
 
 // MARK: - Private func
 private extension SearchMessageView {
-	 func getTimeAsString(timeMs: Int64, includeTime: Bool = false) -> String {
-		let nowTime = Date()
-
-		let inputTime = Date(timeIntervalSince1970: TimeInterval(timeMs / 1000))
-
-		let time = includeTime ? " at \(dateString(date: inputTime, format: "hh:mm aa"))" : ""
-
-		if inputTime.year == nowTime.year
-			&& inputTime.month == nowTime.month
-			&& inputTime.weekOfMonth == nowTime.weekOfMonth {
-			if inputTime.day == nowTime.day {
-				return "Today\(time)"
-			} else if nowTime.day - inputTime.day == 1 {
-				return "Yesterday\(time)"
-			} else {
-				return dateString(date: inputTime, format: "EEE")
-			}
-		} else {
-			return dateString(date: inputTime, format: "yyyy/MM/dd")
-		}
-	}
-
-	 func dateString(date: Date, format: String) -> String {
-		let formatDate = DateFormatter()
-		formatDate.dateFormat = format
-		return formatDate.string(from: date)
-	}
 }
 
 // MARK: - Preview
